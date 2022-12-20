fa9de0bcdcbd951579757005de86ad3d628e0b86
palainp <pierre.alain@tuta.io>
2022-12-20 13:06:17 +0100

    .

--- bindings/crt_init.h
+++ bindings/crt_init.h
@@ -52,5 +52,5 @@ __attribute__((always_inline)) static inline void crt_init_ssp(void)
 __attribute__((always_inline)) static inline void crt_init_tls(void)
 {
 //    (void)platform_set_tls_base(0);
-    (void);
+    (void)0;
 }
